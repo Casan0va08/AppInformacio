ec4524550cb3eb24bc6f92d270e74490660dbae2
Oriol Casacuberta <oriol.casacubertat@educem,net>
2023-11-19 16:31:16 +0100

    Modificacions

--- app/src/main/java/com/example/appinformacio/Cartellera.java
+++ app/src/main/java/com/example/appinformacio/Cartellera.java
@@ -1,18 +1,35 @@
 package com.example.appinformacio;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.TextView;
+import android.content.res.Resources;
+
 
 public class Cartellera extends AppCompatActivity {
+
+    TextView tvAlimanas, tvFNAF, tvSabenAquell, tvJDHambre, tvSawX, tvMarvels;
+    Resources resources = getResources();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cartellera);
 
-        //TODO: Ver si es posible establecer aqui las descripciones o buscar algo similar al *** onLoad ***
+        tvAlimanas      = findViewById(R.id.tvAlimanas);
+        tvFNAF          = findViewById(R.id.tvFNAF);
+        tvSabenAquell   = findViewById(R.id.tvSabenAquell);
+        tvJDHambre      = findViewById(R.id.tvJDHambre);
+        tvSawX          = findViewById(R.id.tvSawX);
+        tvMarvels       = findViewById(R.id.tvMarvels);
 
+        tvAlimanas.setText(getResources().getString(R.string.descripcioAlimanas));
+        tvFNAF.setText(getResources().getString(R.string.descripcioFNAF));
+        tvSabenAquell.setText(getResources().getString(R.string.descripcioSabenAquell));
+        tvJDHambre.setText(getResources().getString(R.string.descripcioJDHambre));
+        tvSawX.setText(getResources().getString(R.string.descripcioSawX));
+        tvMarvels.setText(getResources().getString(R.string.descripcioMarvels));
 
     }
 
